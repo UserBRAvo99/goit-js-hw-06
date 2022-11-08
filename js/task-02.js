0cfcd63f7a7c2f308796210beebe070ae598018b
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2022-11-08 14:39:48 +0200

    2task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// знаходжу список через id
+const list = document.querySelector('#ingredients')
+// цикл для пеебирання масиву ingredients та просвоэння кожному item новий textContent значення
+const ingredientsEl = ingredients.map((element) => {
+  const itemEl = document.createElement('li')
+  itemEl.classList.add('item')
+  itemEl.textContent = `${element}`
+  return itemEl;
+})
+
+// додаю список до тегу ul#ingredients в DOM 
+// через rest
+list.append(...ingredientsEl)
+
