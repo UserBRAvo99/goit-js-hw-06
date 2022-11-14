eec038e8fe575642aa9cee7e25c036de9917ef9c
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2022-11-14 06:15:02 +0200

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formLogin = document.querySelector('.login-form');
+formLogin.addEventListener('submit', formSubmit)
 
+
+function formSubmit(event) {
+    
+    event.preventDefault();
+
+    const formEl = event.currentTarget.elements;
+
+    const emailForm = formEl.email.value;
+    const passwordForm = formEl.password.value
+    ? (emailForm !== "" || passwordForm !== "") : alert('Заповніть будь ласка поля');
+
+    const formData = new FormData(event.currentTarget)
+    formData.forEach((value, name) => {
+        console.log(`${name} - ${value}`);
+    });
+    formLogin.reset()
+}
