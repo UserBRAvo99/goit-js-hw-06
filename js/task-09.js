15f9f7e18e80958ddee1efcf3e42c56a7708fa68
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2022-11-14 06:42:56 +0200

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBnt = document.querySelector('.change-color')
+const colorName = document.querySelector('.color')
+const body = document.querySelector('body')
+
+const changeColorBody = () => { 
+  colorName.textContent = getRandomHexColor()
+  body.style.backgroundColor = getRandomHexColor()
+}
+
+changeColorBnt.addEventListener('click', changeColorBody)
